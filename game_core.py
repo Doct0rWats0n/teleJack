a83aa0b7e10b6c20627e6d1794978e330eb08320
Vlad <ivanov.vlad135@gmail.com>
2022-03-26 14:00:43 +0500

    Made game core

--- game_core.py
+++ game_core.py
@@ -30,10 +30,12 @@ class Card:
         self.suit = suit
         self.nominal = nominal
 
+    def __repr__(self):
+        return f'{self.suit} {self.nominal}'
+
 
 class CardDeck:
     def __init__(self):
-        self.cards = list()
         self.shuffle()
     
     def shuffle(self):
@@ -42,3 +44,69 @@ class CardDeck:
 
     def take_card(self) -> Card:
         return self.cards.pop()
+
+
+class Player:
+    def __init__(self):
+        self.hand = []
+
+    def take_card(self, card: Card):
+        self.hand.append(card)
+
+    def count_card(self) -> int:
+        count = 0
+        for card in self.hand:
+            if card.nominal in (CardNominal.Jack, CardNominal.Queen, CardNominal.King):
+                count += 10
+            if card.nominal == CardNominal.Ace:
+                if count + 11 > 21:
+                    count += 1
+                else:
+                    count += 11
+            else:
+                match card.nominal:
+                    case CardNominal.Two:
+                        count += 2
+                    case CardNominal.Three:
+                        count += 3
+                    case CardNominal.Four:
+                        count += 4
+                    case CardNominal.Five:
+                        count += 5
+                    case CardNominal.Six:
+                        count += 6
+                    case CardNominal.Seven:
+                        count += 7
+                    case CardNominal.Eight:
+                        count += 8
+                    case CardNominal.Nine:
+                        count += 9
+                    case CardNominal.Ten:
+                        count += 10
+        return count
+
+
+class Dealer(Player):
+    def __init__(self):
+        super().__init__()
+
+
+class Game:
+    def __init__(self, bet: int):
+        self.bet = bet
+        self.player = Player()
+        self.dealer = Dealer()
+        self.card_deck = CardDeck()
+        self.start()
+
+    def start(self):
+        for i in range(2):
+            self.player.take_card(self.card_deck.take_card())
+        self.dealer.take_card(self.card_deck.take_card())
+
+    def add_player_card(self):
+        self.player.take_card(self.card_deck.take_card())
+
+    def add_dealer_card(self):
+        while self.dealer.count_card() < 17:
+            self.dealer.take_card(self.card_deck.take_card())
